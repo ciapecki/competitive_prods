a75333eb16889cd692c5c31dc459ba2f7254bdf4
chris <chris@goxp.de>
2008-11-19 16:25:17 +0100

    base TUDLA table changed to lm_lad.tudla_std_prod_final

--- TUDLA/1tudla_base_tables.sql
+++ TUDLA/1tudla_base_tables.sql
@@ -1,16 +1,26 @@
-select * from all_tables@mktgcd_kcierpisz a
-where a.owner = 'LM_LAD'
-and a.table_name like '%TUDLA%'
-
 /*
 Requirements:
 lm_lad.tudla_std_prod_final@mktgcd_kcierpisz
 
 */
 
-select * from lm_lad.tudla_std_prod_final@mktgcd_kcierpisz
+select * from lm_lad.tudla_std_prod_final@mktgcd_kcierpisz a
 
 --- KCIERPISZ
+drop table tudla_std_prod_final_bak;
+rename tudla_std_prod_final to tudla_std_prod_final_bak;
+create table tudla_std_prod_final as
+select distinct duns_number, bi, crm, erp, app_ibm, app_jde, app_microsoft, app_oracle,
+	   app_psft, app_sap, app_siebel, db2, oracle, ms_sql, site_id_number
+from lm_lad.tudla_std_prod_final@mktgcd_kcierpisz a;
+grant select on tudla_std_prod_final to tudla;
+
+/*
+select count(*), count(distinct a.duns_number) from tudla_std_prod_final a -- 33k 32k
+select * from  tudla_std_prod_final a
+where a.db2 + a.ms_sql + a.app_sap + a.app_microsoft + a.app_ibm + a.bi + a.crm + a.erp = 0
+*/
+/*
 rename tudla_final to tudla_final_bak;
 create table tudla_final as
 select * from lm_lad.tudla_final@mktgcd_kcierpisz;
@@ -19,17 +29,27 @@ rename tudla_std_prod to tudla_std_prod_bak;
 create table tudla_std_prod as
 select * from lm_lad.tudla_std_prod@mktgcd_kcierpisz;
 grant select on tudla_std_prod to tudla;
-
+*/
 -- TUDLA
 --drop table tudla_final;
+/*
 drop table tudla_final_bak;
 rename tudla_final to tudla_final_bak;
 create table tudla_final as
 select * from kcierpisz.tudla_final;
 grant select on tudla_final to public;
-
+*/
+/*
 drop tudla_std_prod_bak;
 rename tudla_std_prod to tudla_std_prod_bak;
 create table tudla_std_prod as
 select * from kcierpisz.tudla_std_prod;
 grant select on tudla_std_prod to public;
+*/
+
+drop table tudla_std_prod_final_bak;
+rename tudla_std_prod_final to tudla_std_prod_final_bak;
+create table tudla_std_prod_final as
+select * from kcierpisz.tudla_std_prod_final;
+grant select on tudla_std_prod_final to public;
+
